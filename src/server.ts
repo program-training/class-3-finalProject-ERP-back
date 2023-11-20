cb5f859bada34757cd246d4162dbf32cdf877978
akiva_kraus <akiva1132@gmail.com>
2023-11-20 15:30:28 +0200

    add env

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,20 @@ import {corsOrigin as cors} from './cors/cors';
 import router from "./router";
 import { connectToDatabase } from "./configuration/mongoDB";
 import { ProductModel } from "./configuration/userSchema";
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const port = process.env.PORT;
 const app = express();
 
 app.use(cors);
 app.use(express.json());
 app.use(router)
 
-app.listen(3000, async () => {
+app.listen(port, async () => {
   await connectToDatabase()
-  console.log(`Server is up and running on port 3000`);
+  console.log(`Server is up and running on port ${port}`);
 });
 
 
