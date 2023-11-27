f255cedb8082d4e9f1ae6a96de42dd724fba6133
akiva_kraus <akiva1132@gmail.com>
2023-11-27 09:06:25 +0200

    add tsc

--- src/server.ts
+++ src/server.ts
@@ -14,9 +14,10 @@ app.use(cors)
 app.use(express.json());
 app.use(router)
 
-app.listen(8080, async () => {
+
+app.listen(port, async () => {
   await connectToDatabase()
-  // console.log(`Server is up and running on port ${port}`);
+  console.log(`Server is up and running on port ${port}`);
 });
 
 
