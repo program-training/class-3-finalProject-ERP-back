1c0fdf05a0ab4f456e2bc9f609758f9ff3dcd71d
akiva_kraus <akiva1132@gmail.com>
2023-12-18 12:15:50 +0200

    add throw error in add product

--- src/users/controllers/usersControllers.ts
+++ src/users/controllers/usersControllers.ts
@@ -4,9 +4,9 @@ import { signUp } from "../service/useService";
 import { getToken } from "../../services/apiServices";
 import { handleError } from "../../utils/handleErrors";
 
-export const signUpC = async (args:userData) => {
+export const signUpC = async (args: userData) => {
   console.log(args);
-  
+
   try {
     const user = args
     const newUser = await signUp(user);
@@ -16,14 +16,12 @@ export const signUpC = async (args:userData) => {
     if (error instanceof Error) return error.message
   }
 };
-export const logInC = async (args:any) => {
+export const logInC = async (args: any) => {
   try {
     const user = args;
     const token = await getToken(user);
     return token
   } catch (error) {
-    if (error instanceof Error) return error.message
-    
-    // if (error instanceof Error) return handleError(res, error, 400);
+    throw error
   }
 };
